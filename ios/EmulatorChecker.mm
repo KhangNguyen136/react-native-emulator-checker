0ce1d14fdea533bf6e7559f451368730227ac3d4
Khang Nguyen <khang.nguyen@finviet.com.vn>
2024-07-25 11:06:02 +0700

    disable mainQueueSetup

--- ios/EmulatorChecker.mm
+++ ios/EmulatorChecker.mm
@@ -3,6 +3,11 @@
 @implementation EmulatorChecker
 RCT_EXPORT_MODULE()
 
+(BOOL)requiresMainQueueSetup
+{
+  return NO; // Return YES if your module must be initialized on the main thread
+}
+
 // Example method
 // See // https://reactnative.dev/docs/native-modules-ios
 RCT_EXPORT_METHOD(isEmulator:(RCTPromiseResolveBlock)resolve
